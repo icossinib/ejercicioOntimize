f1cadaf909a32dd20ecf043120dd8fdc6b01fff4
carlosgonzalezcampusdual <carlos.gonzalez@campusdual.com>
2020-05-26 16:41:20 +0200

    changes hasta Usar las peticiones REST

---
+++ ws/src/main/java/com/imatia/ws/core/rest/CandidateRestController.java
@@ -0,0 +1,25 @@
+package com.imatia.ws.core.rest;
+
+
+
+import com.imatia.api.core.service.ICandidateService;
+import com.ontimize.jee.server.rest.ORestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/candidates")
+@ComponentScan(basePackageClasses = { com.imatia.api.core.service.ICandidateService.class })
+public class CandidateRestController extends ORestController<ICandidateService> {
+
+    @Autowired
+    private ICandidateService candidateService;
+
+    @Override
+    public ICandidateService getService() {
+        return this.candidateService;
+    }
+}
