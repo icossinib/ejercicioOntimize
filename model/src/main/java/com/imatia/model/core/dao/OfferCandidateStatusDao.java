a49c9498b7b3584a77e33206c4bb1fd010bd14aa
JoseCabezasPok <jose.cabezas@campusdual.com>
2020-05-29 17:36:58 +0200

    Implementacion que permite gestionar ofertas de trabajo

---
+++ model/src/main/java/com/imatia/model/core/dao/OfferCandidateStatusDao.java
@@ -0,0 +1,16 @@
+package com.imatia.model.core.dao;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+@Repository("OfferCandidateStatusDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/OfferCandidateStatusDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class OfferCandidateStatusDao extends OntimizeJdbcDaoSupport {
+
+    public static final String ATTR_ID ="ID";
+    public static final String ATTR_DESCRIPTION ="DESCRIPTION";
+
+}
